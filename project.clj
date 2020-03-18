bcc8d6e81d49f8118a704372a6abaef15b5be95b
Lillia P <lpodlog@flur.ee>
2020-03-18 14:00:56 -0400

    Fix main function

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Jepsen test for Fluree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main fluree.jepsen
+  :main jepsen.fluree
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [jepsen "0.1.13"]
                  [cheshire "5.6.3"]
